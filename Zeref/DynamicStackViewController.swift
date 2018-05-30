10a839da4c9953007e581bf12b4d33436def225b
Son <trinhson1998@gmail.com>
2018-05-30 16:49:59 +0700

    Zeref

--- Zeref/DynamicStackViewController.swift
+++ Zeref/DynamicStackViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Controller: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
